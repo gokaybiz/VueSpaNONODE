0545936fc380bf30e0ffea323c3ef49547bc4a6f
gokaybiz <gkymhmt@gmail.com>
2020-10-15 02:00:45 +0300

    Update navbar-template.js

--- templates/navbar-template.js
+++ templates/navbar-template.js
@@ -1,8 +1,8 @@
 const NavbarTemplate = `<nav>
 
 <ul>
-  <li><a class="active" href="#/">Home</a></li>
-  <li><a href="#/about">About</a></li>
+  <li><a class="active" href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
 </ul>
 
 </nav>`
